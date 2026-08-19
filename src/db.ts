780a6e23a2134c0475e6606d60827bb40853c004
tiffank1802 <tiffank1802@gmail.com>
2026-08-19 17:33:29 +0000

    suppression du cloud

--- src/db.ts
+++ src/db.ts
@@ -1,5 +1,4 @@
 import Dexie, { type Table } from 'dexie' // ✅ Utilisation de 'type Table' pour verbatimModuleSyntax
-import { dexieCloud } from 'dexie-cloud-addon';
 
 // Setup pour la base de données
 interface SnapshotRecord {
@@ -7,7 +6,7 @@ interface SnapshotRecord {
   data: any // Le snapshot JSON de tldraw
   notebookId: string // lier la sauvegarde à un notebook spécifique
   ownerId?: string // ID de l'utilisateur (optionel)
-  updatedAt:number // Timestamp de la dernière modification
+  timestamp: number // Timestamp de la dernière modification
   
 }
 // 2. Création de la classe de base de données
@@ -16,19 +15,12 @@ class NotesDatabase extends Dexie {
   snapshots!: Table<SnapshotRecord, string>
 
   constructor() {
-    // Ajout de l'addon dexieCloud
-    super('notesDB',{addons:[dexieCloud]})
+    super('notesDB')
     this.version(1).stores({
         // 3. Définition du schéma (version 1)
-        // ON indexe notebookId et updateAt pour pouvoiir faire des requêtes rapides
+        // ON indexe notebookId et timestamp pour pouvoiir faire des requêtes rapides
     // Définir le schéma. Le '!' signifie que ce champ est requis pour le cloud
-      snapshots: 'id, notebookId, updatedAt'
-      // Configurer la connexion au cloud
-    })
-    
-    this.cloud.configure({
-      databaseUrl:'https://z66l856yx.dexie.cloud',
-      requireAuth: false,
+      snapshots: 'id, notebookId, timestamp'
     })
   }
 }
